4b100f86f3552a8ed04d9dbbc9c77565f5adb2c2
Danilo Maldonado <dannsanto@gmail.com>
2023-10-04 18:26:12 -0300

    cadastro de usuário update de validações

--- usuarios/views.py
+++ usuarios/views.py
@@ -16,15 +16,26 @@ def cadastro(request):
         email = request.POST.get('email')
         confirmar_senha = request.POST.get('confirmar_senha')
 
-        if not senha == confirmar_senha:
-            messages.add_message(request, constants.ERROR,
-                                 'As senhas não são iguais')
+        # Validação: As senhas devem ser iguais
+        if senha != confirmar_senha:
+            messages.error(request, 'As senhas não são iguais')
             return redirect('/usuarios/cadastro')
 
+        # Validação: A senha deve ter pelo menos 6 caracteres
         if len(senha) < 6:
-            messages.add_message(request, constants.ERROR,
-                                 'Sua senha deve ter 7 ou mais digitos')
+            messages.error(request, 'Sua senha deve ter 6 ou mais caracteres')
             return redirect('/usuarios/cadastro')
+
+        # Validação: Verifica se o nome de usuário já existe
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Nome de usuário já está em uso')
+            return redirect('/usuarios/cadastro')
+
+        # Validação: Verifica se o email já está cadastrado
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Este email já está cadastrado')
+            return redirect('/usuarios/cadastro')
+
         try:
             user = User.objects.create_user(
                 first_name=primeiro_nome,
